a79a0ea7527dd4a731185053831d9fa66318b92a
Shubham Mankar <themrshubh@gmail.com>
2021-02-28 17:57:17 +0530

    Init Home

---
+++ src/components/ListGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="list-group mx-3">
+    <router-link
+      v-for="a in algorithms"
+      :key="a.link"
+      :to="a.link"
+      class="list-group-item list-group-item-action"
+    >
+      {{ a.name }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        algorithms: [{ name: 'Linear Search', link: '/linear-search' }],
+      }
+    },
+  }
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container">
+      <h2 class="text-center mt-4 fw-light">Algorithms Visualizer</h2>
+      <hr />
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+          <ListGroup />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+  import ListGroup from '../components/ListGroup'
 
-}
+  export default {
+    components: {
+      ListGroup,
+    },
+  }
 </script>
 
-<style>
-
-</style>
+<style></style>
